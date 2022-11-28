753a1c96435cc81b99fca94a9e6376bce71b44ba
lamh85 <lamh85@gmail.com>
2022-11-27 18:19:08 -0800

    Features: upload images,  shuffle playlist, navigate playlist. Add prettier and make prettier fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [isLoadingImages, setIsLoadingImages] = useState(true)
+  const [playlist, setPlaylist] = useState()
+  const [played, setPlayed] = useState()
+  const [viewedPosition, setViewedPosition] = useState()
+
+  const loadingRef = useRef(isLoadingImages)
+  const imageDataUrlRef = useRef(playlist)
+  const viewedPositionRef = useRef(viewedPosition)
+
+  const randomizeArray = (array) => {
+    return array.sort((a, b) => {
+      const randomNumber = Math.random()
+
+      if (randomNumber > 0.5) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+  }
+
+  const handleShuffleClick = (_event) => {
+    setPlaylist(randomizeArray(playlist))
+  }
+
+  const uploadClickHandler = async () => {
+    const dirHandle = await window.showDirectoryPicker()
+
+    const objectUrls = []
+
+    for await (const entry of dirHandle.values()) {
+      const { name } = entry
+      const fileHandle = await dirHandle.getFileHandle(name)
+      const fileData = await fileHandle.getFile()
+      const objectUrl = URL.createObjectURL(fileData)
+
+      objectUrls.push(objectUrl)
+    }
+
+    setPlaylist(objectUrls)
+
+    const initialPosition = Math.round(Math.random() * objectUrls.length)
+    setViewedPosition(initialPosition)
+
+    setIsLoadingImages(false)
+  }
+
+  const navigatePlaylist = (increment) => {
+    const maxPosition = imageDataUrlRef.current.length - 1
+
+    if (increment > 0 && viewedPositionRef.current == maxPosition) {
+      setViewedPosition(0)
+    } else if (increment < 0 && viewedPositionRef.current == 0) {
+      setViewedPosition(maxPosition)
+    } else {
+      setViewedPosition(viewedPositionRef.current + increment)
+    }
+  }
+
+  const keyDownHandler = (event) => {
+    if (loadingRef.current) {
+      return
+    }
+
+    const { code } = event
+
+    let increment = 0
+
+    if (['ArrowDown', 'ArrowRight'].includes(code)) {
+      increment = 1
+    }
+
+    if (['ArrowUp', 'ArrowLeft'].includes(code)) {
+      increment = -1
+    }
+
+    navigatePlaylist(increment)
+  }
+
+  useEffect(() => {
+    loadingRef.current = isLoadingImages
+    imageDataUrlRef.current = playlist
+    viewedPositionRef.current = viewedPosition
+  })
+
+  useEffect(() => {
+    const htmlElem = document.querySelector('html')
+    htmlElem.addEventListener('keydown', keyDownHandler)
+  }, [])
+
+  useEffect(() => {})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ width: '100%', height: '100%' }}
+      // onKeyDown={keyDownHandler}
+    >
+      {!isLoadingImages ? (
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+          <img
+            src={playlist[viewedPosition]}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            onKeyDown={keyDownHandler}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              bottom: 100,
+              left: '50%',
+              transform: 'translateX(-50%)',
+              background: 'white',
+              padding: '1em',
+              fontWeight: 'bold',
+              borderRadius: '1em',
+              opacity: 0.5,
+              display: 'flex',
+            }}
+          >
+            <button onClick={handleShuffleClick}>SHUFFLE</button>
+          </div>
+        </div>
+      ) : (
+        <button onClick={uploadClickHandler}>CLICK ME</button>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
