cb1077d6d5ba5f9c6d26d725f44f56aef00fbb90
lamh85 <lamh85@gmail.com>
2025-05-09 23:47:03 -0700

    Move states: date, objectFit, setObjectFit

--- src/hooks/useMainImage.ts
+++ src/hooks/useMainImage.ts
@@ -1,4 +1,22 @@
+import { useState } from 'react'
 import { Image } from '../types'
+import { fileNameToMoment } from '../helpers/time'
+
+const MONTHS_BY_INDEX = [
+  null,
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
 
 type Options = {
   playlist: number[]
@@ -8,6 +26,7 @@ type Options = {
 
 const useMainImage = (options: Options) => {
   const { playlist, playlistCursor, images } = options
+  const [objectFit, setObjectFit] = useState<'cover' | 'contain'>('cover')
 
   const getMainImage = () => {
     const imageIndex = playlist[playlistCursor]
@@ -15,8 +34,33 @@ const useMainImage = (options: Options) => {
     return images[imageIndex]
   }
 
+  const getDate = () => {
+    const mainImage = getMainImage()
+
+    if (!mainImage) {
+      return ''
+    }
+
+    const fileName = mainImage.name
+
+    const momentDate = fileNameToMoment(fileName)
+    const date = momentDate.date()
+    const monthName = MONTHS_BY_INDEX[momentDate.month()]
+    const year = momentDate.year()
+
+    return `${date} ${monthName} ${year}`
+  }
+
+  const handleToggleObjectFit = () => {
+    const nextState = objectFit === 'contain' ? 'cover' : 'contain'
+    setObjectFit(nextState)
+  }
+
   return {
     mainImage: getMainImage(),
+    date: getDate(),
+    objectFit,
+    setObjectFit: handleToggleObjectFit,
   }
 }
 
--- src/hooks/usePlaylist.ts
+++ src/hooks/usePlaylist.ts
@@ -1,22 +1,5 @@
 import { useEffect, useState } from 'react'
-import { fileNameToMoment } from '../helpers/time'
 import { Image } from '../types'
-
-const MONTHS_BY_INDEX = [
-  null,
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
 
 type NavigateArgsT = {
   increment: number
@@ -35,7 +18,6 @@ function usePlaylist(images: Image[]) {
   // playlist = [2, 0, 1]
   const [playlist, setPlaylist] = useState<number[] | []>([])
 
-  const [objectFit, setObjectFit] = useState<'cover' | 'contain'>('cover')
   const [dateSorting, setDateSorting] = useState<'asc' | 'desc' | 'random'>(
     'asc'
   )
@@ -57,28 +39,6 @@ function usePlaylist(images: Image[]) {
         blob: images[imageIndex].blob,
       }
     })
-  }
-
-  const getDate = () => {
-    const mainImage = getMainImage()
-
-    if (!mainImage) {
-      return ''
-    }
-
-    const fileName = mainImage.name
-
-    const momentDate = fileNameToMoment(fileName)
-    const date = momentDate.date()
-    const monthName = MONTHS_BY_INDEX[momentDate.month()]
-    const year = momentDate.year()
-
-    return `${date} ${monthName} ${year}`
-  }
-
-  const handleToggleObjectFit = () => {
-    const nextState = objectFit === 'contain' ? 'cover' : 'contain'
-    setObjectFit(nextState)
   }
 
   const handleSortDate = (override?: 'asc' | 'desc') => {
@@ -213,14 +173,11 @@ function usePlaylist(images: Image[]) {
     playlist,
     playlistCursor,
     thumbnails: getThumbnails(),
-    date: getDate(),
-    objectFit,
     dateSorting,
     keyDownHandler,
     sort: handleSortDate,
     navigate: navigatePlaylist,
     navigateToDate,
-    setObjectFit: handleToggleObjectFit,
     randomizeSort: handleShuffleClick,
   }
 }
