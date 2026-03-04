671145f93f476efc0b654a03e760fb0bb60610ca
lamh85 <lamh85@gmail.com>
2026-03-03 21:23:13 -0800

    ImageInfo component: template code

---
+++ src/SlideshowPage/ImageInfo/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function ImageInfo() {
+  return (
+    <div className="image-info">
+      <div className="city-name">Brussels</div>
+      <img
+        className="flag"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_Belgium_%28civil%29.svg/330px-Flag_of_Belgium_%28civil%29.svg.png"
+      />
+      <div className="date">28 August 2022</div>
+    </div>
+  )
+}
